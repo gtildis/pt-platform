aa95b3f5cf31fcfed4aa04f70d314465ab94b1f8
gtildis <gretil_15@hotmail.com>
2024-04-28 15:12:12 +0200

    Add Create Form and toast

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-	return <div>page</div>;
+	return (
+		<div>
+			<Link href="/teacher/create">
+				<Button className="p-6">New Course</Button>
+			</Link>
+		</div>
+	);
 };
 
 export default CoursesPage;
