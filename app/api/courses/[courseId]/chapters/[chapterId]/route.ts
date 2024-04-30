2be1809e17284ee00c0827ae46afa4da4f26161d
gtildis <gretil_15@hotmail.com>
2024-04-30 17:59:15 +0200

    add Chapter Creation and Chapter title

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -2,18 +2,18 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function PUT(
+export async function PATCH(
 	req: Request,
-	{ params }: { params: { courseId: string } }
+	{ params }: { params: { courseId: string; chapterId: string } }
 ) {
 	try {
 		const { userId } = auth();
+		const { ...values } = await req.json();
+		// const { isPublished, ...values } = await req.json();
 
 		if (!userId) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
-
-		const { list } = await req.json();
 
 		const courseOwner = await db.course.findUnique({
 			where: {
@@ -26,16 +26,21 @@ export async function PUT(
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
 
-		for (let item of list) {
-			await db.chapter.update({
-				where: { id: item.id },
-				data: { position: item.position },
-			});
-		}
+		const chapter = await db.chapter.update({
+			where: {
+				id: params.chapterId,
+				courseId: params.courseId,
+			},
+			data: {
+				...values,
+			},
+		});
 
-		return new NextResponse("Success", { status: 200 });
+		//TODO: Handle video upload
+
+		return NextResponse.json(chapter);
 	} catch (error) {
-		console.log("[REORDER]", error);
+		console.log("[COURSES_CHAPTER_ID]", error);
 		return new NextResponse("Internal Error", { status: 500 });
 	}
 }
