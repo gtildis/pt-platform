5b04394d4ee1106cc0bcde08f5081dc6a049e34f
gtildis <gretil_15@hotmail.com>
2024-04-30 12:47:03 +0200

    add attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,12 +3,18 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+	CircleDollarSign,
+	File,
+	LayoutDashboard,
+	ListChecks,
+} from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -20,6 +26,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const course = await db.course.findUnique({
 		where: {
 			id: params.courseId,
+		},
+		include: {
+			attachments: {
+				orderBy: {
+					createdAt: "desc",
+				},
+			},
 		},
 	});
 
@@ -82,11 +95,21 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 						</div>
 						<div>TODO: Chapters</div>
 					</div>
-					<div className="flex items-center gap-x-2">
-						<IconBadge icon={CircleDollarSign} />
-						<h2 className="text-xl">Sell your course</h2>
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={CircleDollarSign} />
+							<h2 className="text-xl">Sell your course</h2>
+						</div>
+
+						<PriceForm initialData={course} courseId={course.id} />
 					</div>
-					<PriceForm initialData={course} courseId={course.id} />
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={File} />
+							<h2 className="text-xl">Resources & Attachments</h2>
+						</div>
+						<AttachmentForm initialData={course} courseId={course.id} />
+					</div>
 				</div>
 			</div>
 		</div>
